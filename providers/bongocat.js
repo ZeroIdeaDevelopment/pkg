28b481bc4104b2fe66ea42a0e0dbd5628ba0d287
ry00001 <ry00001@protonmail.com>
2018-09-21 23:08:42 +0100

    better bongocat

--- providers/bongocat.js
+++ providers/bongocat.js
@@ -3,11 +3,11 @@ const Provider = require('../Provider')
 module.exports = class extends Provider {
     constructor() {
         super();
-        this.name = 'Bongo Cat'
+        this.name = 'the almighty bongo cat'
         this.logo = null;
     }
 
     execute(msg, args) {
-        msg.channel.createMessage(`the bongo cat bongoes at ${args.join(' ')}`);
+        msg.channel.createMessage('<a:bongocat:492818212187144214>');
     }
 }
