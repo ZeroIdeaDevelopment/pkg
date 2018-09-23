75bb678f19878063b7f22b46178b9d8ee294fc5d
Lewis Crichton <lewistehminerz@outlook.com>
2018-09-23 11:14:59 +0100

    AUR

---
+++ providers/aur.js
@@ -0,0 +1,47 @@
+const fetch = require('node-fetch');
+const fs = require('fs');
+const path = require('path');
+const Provider = require('../Provider');
+
+module.exports = class extends Provider {
+    constructor() {
+        super();
+        this.name = 'Arch User Repository';
+        this.logo = fs.readFileSync(path.resolve('./img/aur.png'));
+    }
+
+    async execute(msg, args) {
+        let results = await fetch('https://aur.archlinux.org/rpc/?v=5&type=search&by=name&arg=' + args.map(a => encodeURIComponent(a)).join('+'));
+        let json = await results.json();
+        if (!json) {
+            await msg.channel.createMessage('<:icerror:435574504522121216>  |  API error! :(');
+        } else {
+            if (json.resultCount < 1) {
+                await msg.channel.createMessage('<:icerror:435574504522121216>  |  No packages found.');
+            } else {
+                let pkg = json.results[0];
+                await msg.channel.createMessage({
+                    embed: {
+                        title: pkg.Name,
+                        url: 'https://aur.archlinux.org/packages/' + pkg.Name,
+                        description: pkg.Description,
+                        fields: [
+                            {
+                                name: 'Latest Version',
+                                value: pkg.Version,
+                                inline: true
+                            },
+                            {
+                                name: 'Maintainer',
+                                value: pkg.Maintainer,
+                                inline: true
+                            }
+                        ],
+                        color: 0x1793D1,
+                        thumbnail: { url: 'attachment://logo.png' }
+                    }
+                }, { file: this.logo, name: 'logo.png' });
+            }
+        }
+    }
+}
